581b7736cd21221ab2826ff845d53fd3ac0d4075
alex1431999 <ah01242@surrey.ac.uk>
2019-11-28 16:50:35 +0000

    Add Username and Password input to login

--- src/scenes/Login/components/FormField/index.js
+++ src/scenes/Login/components/FormField/index.js
@@ -4,6 +4,9 @@ import ButtonLogIn from './components/ButtonLogIn'
 function FormField() {
   return (
     <div className="FormField">
+      <input type="text" placeholder="Username"/>
+      <br/>
+      <input type="password" placeholder="Password"/>
       <ButtonLogIn/>
     </div>
   )
