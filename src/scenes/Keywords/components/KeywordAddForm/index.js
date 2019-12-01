1a847cbc3b106775354187fd6ca29199b3e2fe88
alex1431999 <ah01242@surrey.ac.uk>
2019-12-01 22:00:38 +0000

    Add boilerplate

---
+++ src/scenes/Keywords/components/KeywordAddForm/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class KeywordAddForm extends React.Component {
+  render() {
+    return (
+      <div className="KeywordAddForm">
+        
+      </div>
+    )
+  }
+}
+
+export default KeywordAddForm;
---
+++ src/scenes/Keywords/components/KeywordSearch/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class KeywordSearch extends React.Component {
+  render() {
+    return (
+      <div className="KeywordSearch">
+        
+      </div>
+    )
+  }
+}
+
+export default KeywordSearch;
