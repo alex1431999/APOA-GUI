9b8491ddc933f9e06e93c8fbf7f5b85471da115f
alex1431999 <ah01242@surrey.ac.uk>
2019-12-01 22:01:49 +0000

    Add Keywords component to scene

--- src/scenes/Keywords/index.js
+++ src/scenes/Keywords/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import KeywordAddForm from './components/KeywordAddForm/index'
+import KeywordSearch from './components/KeywordSearch/index'
+import KeywordList from './components/KeywordList/index'
+
 function Keywords() {
   return (
     <div className="Keywords center-screen">
+      <KeywordAddForm/>
+      <KeywordSearch/>
+      <KeywordList/>
     </div>
   )
 }
