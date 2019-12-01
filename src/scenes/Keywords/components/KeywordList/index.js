3103f7512caaeabdcc6f8bd591698f4effda5e1c
alex1431999 <ah01242@surrey.ac.uk>
2019-12-01 17:21:01 +0000

    Add basic Keywords scene structure

---
+++ src/scenes/Keywords/components/KeywordList/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class KeywordList extends React.Component {
+  render() {
+    return (
+      <div className="KeywordList">
+        
+      </div>
+    )
+  }
+}
+
+export default KeywordList;
---
+++ src/scenes/Keywords/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Keywords() {
+  return (
+    <div className="Keywords center-screen">
+    </div>
+  )
+}
+
+export default Keywords;
