7ff2eee17d6bf18e41cf6682edfbf550c24f1589
alex1431999 <ah01242@surrey.ac.uk>
2019-12-01 23:53:42 +0000

    Display keywords as list on keywords scene

--- src/scenes/Keywords/components/KeywordList/index.js
+++ src/scenes/Keywords/components/KeywordList/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+import apiService from '../../../../services/api/index'
+import store from '../../../../store';
+
 class KeywordList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keywords: []
+    }
+
+    store.subscribe(() => this.setState({keywords: store.getState().keywordManager.keywords}));
+
+    apiService.getKeywords();
+  }
+
   render() {
     return (
       <div className="KeywordList">
-        
+        <ul>
+          {
+            this.state.keywords.map((keyword, key) => {
+              return <li key={key}>{keyword.keyword_string} ({keyword.language})</li>
+            })
+          }
+        </ul>
       </div>
     )
   }
