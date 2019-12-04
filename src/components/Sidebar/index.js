119d2989da1a2f3a9632e62f0922490a25065a47
alex1431999 <ah01242@surrey.ac.uk>
2019-12-04 23:20:21 +0000

    Hide sidebar when not logged in

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,23 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import store from '../../store'
+
 import './styles.scss'
 
-function Sidebar() {
-  return (
-    <div className="Sidebar wrapper">
-      <nav>
-        <div className="sidebar-header">
-          <h3>Menue</h3>
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: localStorage.getItem('username'),
+    }
+
+    store.subscribe(() => {
+      this.setState({
+        username: store.getState().authenticator.username,
+      })
+    });
+  }
+
+  render() {
+    if (this.state.username) {
+      return (
+        <div className="Sidebar wrapper">
+          <nav>
+            <div className="sidebar-header">
+              <h3>Menue</h3>
+            </div>
+            <ul className="list-unstyled components">
+              <li className="active">
+                <Link to="/keywords">Keywords</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <ul className="list-unstyled components">
-          <li className="active">
-            <Link to="/keywords">Keywords</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
+      )
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Sidebar
