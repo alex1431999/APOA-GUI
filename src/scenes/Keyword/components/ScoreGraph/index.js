2311e09ef826fa0f033033f4cfbf3cc088b0c5f7
alex1431999 <ah01242@surrey.ac.uk>
2020-03-20 23:52:12 +0000

    Update Keyword page
    Accumulate plotting points to show an average instead of all points

--- src/scenes/Keyword/components/ScoreGraph/index.js
+++ src/scenes/Keyword/components/ScoreGraph/index.js
@@ -12,6 +12,7 @@ class ScoreGraph extends React.Component {
 
     this.state = {
       status: 'loading',
+      pointsAmount: 20,
       plottingData: null,
     }
 
@@ -29,16 +30,43 @@ class ScoreGraph extends React.Component {
       });
   }
 
+  accumulatePlottingData(plottingData) {
+    if (plottingData.length === 0) {
+      return [];
+    }
+
+    const pointsAmountToAccumulate = Math.ceil(plottingData.length / this.state.pointsAmount);
+
+    const plottingDataAccumulated = [];
+    let accumulator = 0;
+    let point = plottingData[0];
+    for (let i = 0; i < plottingData.length; i += 1) {
+      if (i % pointsAmountToAccumulate === 0 && i !== 0) {
+        point.score = accumulator / pointsAmountToAccumulate;
+        plottingDataAccumulated.push(point);
+        accumulator = 0;
+        point = plottingData[i];
+      }
+
+      accumulator += plottingData[i].score;
+    }
+
+    return plottingDataAccumulated;
+  }
+
   render() {
     let content = <BasicLoader/>;
 
     if (this.state.plottingData) {
+      /* Accumulate plotting data points*/
+      const plottingDataAccumulated = this.accumulatePlottingData(this.state.plottingData);
+
       /* Format data */
-      const formattedData = {};
-      this.state.plottingData.forEach((point) => formattedData[point.timestamp] = point.score);
+      const plottingDataFormatted = {};
+      plottingDataAccumulated.forEach((point) => plottingDataFormatted[point.timestamp] = point.score);
 
       /* Plot graph */
-      const graph = <LineChart round={2} min={-1} max={1} xtitle="Timestamp" ytitle="Sentiment" data={formattedData}></LineChart>
+      const graph = <LineChart round={2} min={-1} max={1} xtitle="Timestamp" ytitle="Sentiment" data={plottingDataFormatted}></LineChart>
       content = graph;
     }
 
